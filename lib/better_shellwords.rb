11a748f3a7abe86602f95929b4158ea4b523a0d7
Ben Pickles <spideryoung@gmail.com>
2016-06-24 21:18:11 +0100

    Initial commit.

---
+++ lib/better_shellwords.rb
@@ -0,0 +1,49 @@
+require 'strscan'
+
+class BetterShellwords
+  NON_WHITESPACE = /\S+/
+  WHITESPACE = /\s+/
+
+  QUOTES = {
+    '"' => /"[^"]+"/,
+    "'" => /'[^']+'/,
+  }
+
+  def initialize(string)
+    @scanner = StringScanner.new(string.strip)
+  end
+
+  def split
+    words = []
+
+    while !scanner.eos?
+      next_char = scanner.peek(1)
+
+      if next_char == '"' || next_char == "'"
+        end_of_quote = scanner.match?(QUOTES.fetch(next_char))
+
+        if end_of_quote
+          scanner.pos += 1
+          words << scanner.peek(end_of_quote - 2)
+          scanner.pos += end_of_quote - 1
+          next
+        end
+      end
+
+      scanned = scanner.scan(NON_WHITESPACE)
+
+      if scanned
+        words << scanned
+      elsif scanner.scan(WHITESPACE)
+        # Skip to the next non-whitespace.
+      else
+        raise 'bang'
+      end
+    end
+
+    words
+  end
+
+  private
+    attr_reader :scanner
+end
---
+++ spec/better_shellwords_spec.rb
@@ -0,0 +1,82 @@
+require 'better_shellwords'
+
+RSpec.describe BetterShellwords do
+  describe '.split' do
+    subject { BetterShellwords.new(string).split }
+
+    context 'empty' do
+      let(:string) { '' }
+      it { should eql([]) }
+    end
+
+    context 'simple' do
+      let(:string) { 'simple' }
+      it { should eql(['simple']) }
+    end
+
+    context 'multi-word' do
+      let(:string) { 'multi-word simple' }
+      it { should eql(['multi-word', 'simple']) }
+    end
+
+    context 'with non-word characters' do
+      let(:string) { ',hello,there...how_are;you?' }
+      it { should eql([',hello,there...how_are;you?']) }
+    end
+
+    context 'single "' do
+      let(:string) { '"' }
+      it { should eql(['"']) }
+    end
+
+    context "single '" do
+      let(:string) { "'" }
+      it { should eql(["'"]) }
+    end
+
+    context '"quoted"' do
+      let(:string) { ' "quoted  term" ' }
+      it { should eql(['quoted  term']) }
+    end
+
+    context '"quoted" and unquoted' do
+      let(:string) { 'a "quoted  term" and more' }
+      it { should eql(['a', 'quoted  term', 'and', 'more']) }
+    end
+
+    context '"unbalanced quote' do
+      let(:string) { 'oh "dear what can the matter be' }
+      it { should eql(['oh', '"dear', 'what', 'can', 'the', 'matter', 'be']) }
+    end
+
+    context 'another unbalanced" quote' do
+      let(:string) { 'another unbalanced" quote' }
+      it { should eql(['another', 'unbalanced"', 'quote']) }
+    end
+
+    context "'quoted'" do
+      let(:string) { " 'quoted  term' " }
+      it { should eql(['quoted  term']) }
+    end
+
+    context "'quoted' and unquoted" do
+      let(:string) { "a 'quoted  term' and more" }
+      it { should eql(['a', 'quoted  term', 'and', 'more']) }
+    end
+
+    context "'unbalanced quote" do
+      let(:string) { "oh 'dear what can the matter be" }
+      it { should eql(['oh', "'dear", 'what', 'can', 'the', 'matter', 'be']) }
+    end
+
+    context "another unbalanced' quote" do
+      let(:string) { "another unbalanced' quote" }
+      it { should eql(['another', "unbalanced'", 'quote']) }
+    end
+
+    context 'mixed quotes' do
+      let(:string) { %( '"quoted  "term"' and "more ''like" this ) }
+      it { should eql(['"quoted  "term"', 'and', "more ''like", 'this']) }
+    end
+  end
+end
